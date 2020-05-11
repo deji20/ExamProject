4c3b2e21359d76d4623598be1b83b8dc951ba3f6
Deji <ehinlanwo.deji@live.dk>
2020-05-11 13:03:45 +0200

    Initial commit

---
+++ src/main/java/com/exam/mobilehomes/MobilehomesApplication.java
@@ -0,0 +1,13 @@
+package com.exam.mobilehomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobilehomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobilehomesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/mobilehomes/MobilehomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.mobilehomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobilehomesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
